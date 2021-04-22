8bb10307315f750d0932cbbdeb98f2d0dedc19da
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2021-04-21 18:44:19 -0700

    attribution

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 /*
 ; ==============================
 ; Title: main.ts
@@ -8,17 +7,6 @@
 ; Description: This is the main.ts file
 ; ==============================
 */
-=======
-/**
- * Title: main.ts
- * Author: Professor Krasso
- * Date: 19 April 2021
- * Modified By: Juvenal Gonzalez
- * Description: main ts file
- */
-
-
->>>>>>> Stashed changes
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
